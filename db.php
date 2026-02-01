77a39df7e5bb727ac859ac5af242ee3c8278a856
Lukáš Odler <lukas.odler@gmail.com>
2026-02-02 00:02:03 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,15 @@
 <?php
-// Súbor db.php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "gaming_eshop"; // Uisti sa, že toto sedí s phpMyAdmin
+$dbname = "gaming_eshop";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Spojenie zlyhalo: " . $conn->connect_error);
 }
-// SEM NEDÁVAJ session_start()!
+
+
 ?>
