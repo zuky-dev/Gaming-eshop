dfc9b7f446b734132dbde5d8b23e0d6a9273dad0
Lukáš Odler <lukas.odler@gmail.com>
2026-02-02 00:09:43 +0100

    Update pridat_do_kosika.php

--- pridat_do_kosika.php
+++ pridat_do_kosika.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-// Zmenené na $_POST, pretože tak to posielaš z detailu
+
 if (isset($_POST['product_id'])) {
     $id = intval($_POST['product_id']);
     
@@ -16,7 +16,7 @@ if (isset($_POST['product_id'])) {
     }
 }
 
-// Presmerovanie do košíka, aby používateľ videl, že sa to podarilo
-// Namiesto fixného Location: kosik.php dáme:
+
 header("Location: " . $_SERVER['HTTP_REFERER']);
+
 exit();
